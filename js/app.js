c56a2697ee012b45742e64e5970f6c7659b5e0fd
Stephen Rimbakusumo <srimbakusumo@gmail.com>
2015-10-21 23:52:08 -0700

    Testing more routing

--- js/app.js
+++ js/app.js
@@ -7,22 +7,25 @@ var debugMsg = function(msg) {
 
 var ngApp = angular.module('ngApp', ['ngRoute']);
 
-ngApp.config(function($routeProvider) {
+ngApp.config('$routerProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'js/views/home.html',
             controller: 'CtrlHome'
         })
-        .when('/about/', {
+        .when('/about', {
             templateUrl: 'views/about.html',
             controller: 'CtrlAbout'
         })
-        .when('/projects/', {
+        .when('/projects', {
             templateUrl: 'views/projects.html',
             controller: 'CtrlProjects'
         })
-        .when('/resume/', {
+        .when('/resume', {
             templateUrl: 'views/resume.html',
             controller: 'CtrlResume'
         });
+
+    //check browser support
+    $locationProvider.html5Mode(true);
 });
